36f5c957112a2f186822a22c29c8208e87ddb383
Adrian Gette <adrian.gette@icloud.com>
2021-08-11 20:11:05 -0300

    Hacer que se reciba una base desde la clase TypeScript al componente en html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string  = 'App Contador';
   numero: number = 0;
+  base : number = 5;
 
   acumular( valor: number ) {
     this.numero += valor;
